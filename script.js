19acb2bfee19350a2bcf07182d991adba8b16a62
robingusain <156085704+robingusain@users.noreply.github.com>
2024-04-10 19:35:45 +0530

    added script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+function leetcodeProfile() {
+    window.location.assign("https://leetcode.com/_spec_classy/");
+}
+function githubProfile() {
+    window.location.assign("https://github.com/robingusain");
+}
